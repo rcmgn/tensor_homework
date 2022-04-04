83b0433647d35d3c61d6537daa1df8a83cedd212
rcmgn <rcmgn@yandex.ru>
2022-04-04 16:16:45 +0500

    Add Task5 and task 7

---
+++ 06_monitoring/task7/win_ram.py
@@ -0,0 +1,12 @@
+#!/bin/env python3
+import psutil
+import datetime, time
+
+cpu = psutil.cpu_percent()
+timestamp = time.mktime(datetime.datetime.now().timetuple())
+total_ram = psutil.virtual_memory().available // 1000000
+used_ram = psutil.virtual_memory().used // 1000000
+free_ram = psutil.virtual_memory().free // 1000000
+
+print("ram_stat,main_host=server1 total_ram={},used_ram={},free_ram={} {}".\
+		format(total_ram, used_ram, free_ram, timestamp))
